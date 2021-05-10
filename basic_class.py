3f87b30b0e2bb96cce817434215298ff2b48b62c
LuChin Chang <leochang0000@outlook.com>
2021-05-10 16:37:31 +0800

    basic_class

--- basic_class.py
+++ basic_class.py
@@ -1,6 +1,26 @@
 class table():
-    def __init__(self, name, primary, attributes):
+    def __init__(self, name, attributes, primary = 0):
+        # table_name
+        # index of attribute of primary key
+        # array of column instances
         self.name = name
         self.primary = primary
         self.attributes = attributes
 
+class column():
+    def __init__(self, name, is_unique, d_type, length):
+        # column_name
+        # is_unique = true or false
+        # data_type = 'int', 'float', 'char(n)'
+        # length = number of characters
+        self.name = name
+        self.is_unique = is_unique
+        self.d_type = d_type
+        self.length = length
+
+
+
+
+
+if __name__ == "__main__":
+    pass
